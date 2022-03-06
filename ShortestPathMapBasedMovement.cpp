839b0346db1c9eddde52acce761bb3cc0a510d4a
Kamalesh <kamaleshmahata8348@gmail.com>
2022-03-06 22:03:38 +0530

    Dijkstra Algo

---
+++ ShortestPathMapBasedMovement.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+#include<queue>
+int main(){
+cout<<"Please insert initial_coordinate";
+int x, y, z, n;
+cout<<"Please insert initial X coordinate";
+cin >> x;
+cout<<"Please insert initial Y coordinate";
+cin >> y;
+cout<<"Please insert initial Z coordinate";
+cin >> z;
+cout<<"Please insert the no of node";
+cin >> n;
+
+for (int i = 0; i < n; i++)
+{
+    int unvInitial_coordinate_x = NULL;
+    int unvInitial_coordinate_y = NULL;
+    int unvInitial_coordinate_z = NULL;
+
+    int V1[] = {unvInitial_coordinate_x,unvInitial_coordinate_y, unvInitial_coordinate_z};
+    int X[] = {x,y,z};
+    int sizeOfX = sizeof(X);
+    int V2[] = {};
+    for (int i = 0; i < sizeOfX; i++)
+    {
+        V2[i] = X[i];
+    }
+
+
+
+    
+    
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
